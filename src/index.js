18580fef2bad9c4da1e2608e11915bee9a4042e0
Angelo Velasquez <angeloquez1782@hotmail.com>
2023-06-28 06:58:13 -0500

    defini la variable port para el puerto en que va a estar el servidor

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("Helo perra")
+import app from "./app"
+
+app.listen(app.get("port")) //aqui uso la variable port que ya definimos en index
+
+console.log("Server on port", app.get("port"))
